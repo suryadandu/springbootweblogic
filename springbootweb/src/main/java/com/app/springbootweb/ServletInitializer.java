f40a7e8e82de4146dfc70e0978cc007454a13b97
raju <brajuautomation@gmail.com>
2019-12-26 22:11:39 +0530

    spring boot weblogic demo

---
+++ springbootweb/src/main/java/com/app/springbootweb/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.app.springbootweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootwebApplication.class);
+	}
+	
+	
+
+}
---
+++ springbootweb/src/test/java/com/app/springbootweb/SpringbootwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
